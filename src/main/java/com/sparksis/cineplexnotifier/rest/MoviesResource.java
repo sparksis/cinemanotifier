400bb09d2960deb41baf52755d5ad4b1ae9870c8
colton <colton@sparksis.com>
2016-10-08 02:49:15 -0600

    Added a very basic rest service to accomodate page structure Added very basic angular application (inline script will need serious refactoring when complete)

---
+++ src/main/java/com/sparksis/cineplexnotifier/rest/MoviesResource.java
@@ -0,0 +1,22 @@
+package com.sparksis.cineplexnotifier.rest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("movies")
+@Produces(MediaType.APPLICATION_JSON)
+public class MoviesResource {
+
+	@GET
+	@Path("/")
+	public List<String> getAllMovies() {
+
+		return Arrays.asList("Doctor Strange");
+	}
+
+}
---
+++ src/main/java/com/sparksis/cineplexnotifier/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package com.sparksis.cineplexnotifier.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RestApplication extends Application {
+
+}
