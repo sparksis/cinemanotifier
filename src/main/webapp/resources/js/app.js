773211cb88617daeebcabc3601416b43141cf788
colton <colton@sparksis.com>
2016-10-11 03:46:54 -0600

    refactored Movies service into a more appropriate location

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,9 +1,5 @@
 angular.module('moviesApp', [ 'ngResource', 'ngRoute' ]);
 
-angular.module('moviesApp').factory('Movies', function($resource) {
-	return $resource('/rest/movies');
-});
-
 angular.module('moviesApp').controller('MoviesController',
 		function($scope, Movies) {
 			$scope.movies = Movies.query();
