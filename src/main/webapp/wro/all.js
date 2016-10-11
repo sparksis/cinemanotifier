be1ce2451aa6ae07e8b6fff18416f2fa8ce4af95
colton <colton@sparksis.com>
2016-10-11 05:02:46 -0600

    removing overcommited wro file

--- src/main/webapp/wro/all.js
+++
@@ -1,8 +0,0 @@
-angular.module('emailSignup', ['ngResource']);
-
-angular.module('emailSignup').component('emailSignup', {
-	templateUrl : 'resources/js/email-signup/email-signup.template.html',
-	controller : function() {
-		
-	}
-});
