c63d0060b743d00ab1f7b4fb46d9f59038c5dd9e
colton <colton@sparksis.com>
2016-10-11 05:00:53 -0600

    implementing the wro filter to cleanup javascript imports

---
+++ src/main/webapp/wro/all.js
@@ -0,0 +1,8 @@
+angular.module('emailSignup', ['ngResource']);
+
+angular.module('emailSignup').component('emailSignup', {
+	templateUrl : 'resources/js/email-signup/email-signup.template.html',
+	controller : function() {
+		
+	}
+});
