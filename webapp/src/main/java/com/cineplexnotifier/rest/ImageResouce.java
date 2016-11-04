5455a8ce81f2cde503fc5564b8c2a688ec22d430
colton <colton@sparksis.com>
2016-11-04 00:48:05 -0600

    handle Cineplex logo in 404

--- webapp/src/main/java/com/cineplexnotifier/rest/ImageResouce.java
+++ webapp/src/main/java/com/cineplexnotifier/rest/ImageResouce.java
@@ -1,6 +1,9 @@
 package com.cineplexnotifier.rest;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import javax.ejb.EJB;
@@ -8,6 +11,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.RedirectionException;
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.annotations.cache.Cache;
@@ -17,16 +22,29 @@ import com.cineplexnotifier.data.MovieRepository;
 @Path("images")
 @Produces("image/jpeg")
 public class ImageResouce {
-	
+
+	public static final String MISSING_THUMBNAIL_URL = "http://mediafiles.cineplex.com/Cineplex2013/postermissing_230x341.jpg";
+
 	@EJB
 	private MovieRepository dao;
-	
+
 	@GET
 	@Path("thumbnail/{cineplexKey}")
 	@Cache
-	public byte[] getThumbnail(@PathParam("cineplexKey") String key) throws IOException {
-		URL remoteUrl = new URL(dao.selectByCineplexKey(key).getThumbnailImageUrl());
-		return IOUtils.toByteArray(remoteUrl.openStream());
+	public byte[] getThumbnail(@PathParam("cineplexKey") String key) throws IOException, URISyntaxException {
+		String remoteUrl = dao.selectByCineplexKey(key).getThumbnailImageUrl();
+
+		// Do not deliver the not found images from our server as they contain
+		// the Cineplex logo
+		if (MISSING_THUMBNAIL_URL.equals(remoteUrl)) {
+			throw new RedirectionException(Status.MOVED_PERMANENTLY, new URI(remoteUrl));
+		}
+
+		URL remote = new URL(remoteUrl);
+		InputStream stream = remote.openStream();
+		byte[] r = IOUtils.toByteArray(stream);
+		IOUtils.closeQuietly(stream);
+		return r;
 	}
 
 }
