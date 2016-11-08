13bcc86cd66104c207e3954bd06e584de8a94d26
colton <colton@sparksis.com>
2016-11-07 17:21:37 -0700

    Created RedirectionExceptionMapper
    Suppresses unnecessary logging of Redirect exceptions

---
+++ webapp/src/main/java/com/cineplexnotifier/rest/RedirectionExceptionMapper.java
@@ -0,0 +1,21 @@
+package com.cineplexnotifier.rest;
+
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.RedirectionException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+/**
+ * Suppress logging of RedirectExceptions to console
+ * 
+ * @author colton
+ *
+ */
+@Provider
+public class RedirectionExceptionMapper implements ExceptionMapper<RedirectionException> {
+
+	@Override
+	public Response toResponse(RedirectionException exception) {
+		return exception.getResponse();
+	}
+}
